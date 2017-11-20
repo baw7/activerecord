1f2843b5155bc9605f0e979b6e71dde20eedeada
baw7 <31857911+baw7@users.noreply.github.com>
2017-11-19 21:29:04 -0500

    Update todo.php

--- todo.php
+++ todo.php
@@ -1,1 +1,17 @@
 <?php
+class todo extends model {
+    public $id;
+    public $owneremail;
+    public $ownerid;
+    public $createddate;
+    public $duedate;
+    public $message;
+    public $isdone;
+    public $isdone;
+    
+    public static function getTablename(){
+        $tableName='todos';
+        return $tableName;
+}
+}
+?>
